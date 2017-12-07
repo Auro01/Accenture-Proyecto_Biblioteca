562eb08042877e5ddd16544912fa755ad879d043
Eduardo Ancira Salazar <eanciras@gmail.com>
2017-12-07 00:33:44 -0600

    Documentacion Inicial y Metodos de Impresion
    documentacion de las clases y sus parametros asi commo metodos de impresion de sus datos

--- Proyecto.Biblioteca/src/proyecto/biblioteca/Estado.java
+++ Proyecto.Biblioteca/src/proyecto/biblioteca/Estado.java
@@ -3,23 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto.biblioteca;
 
 /**
  *
- * @author Adrian
+ * @author Java
  */
 public class Estado {
     private String sNombreEstado;
     private int iIdEstado;
     private Gestor_Biblioteca gBibBibliotecasActuales;
-    
+    /*
+    * Estado
+    *
+    * Metodo para la creacion de un objecto Estado dados sin parametros.
+     */
     public Estado() {
         this.sNombreEstado = "";
         this.iIdEstado = -1;
         this.gBibBibliotecasActuales = new Gestor_Biblioteca();
     }
-    
+
+    /*
+    * Estado
+    *
+    * Metodo para la creacion de un objecto Estado dados ciertos parametros.
+    * @param sNombreEstado es el <code>Nombre<code> del objecto.
+    * @param iIdEstado es el <code>Id<code> del objecto.
+    * @param gBibBibliotecasActuales es el <code>Gestor de Bibliotecas<code> del objecto.
+     */
     public Estado(String sNombreEstado, int iIdEstado,Gestor_Biblioteca gBibBibliotecasActuales) {
         this.sNombreEstado = sNombreEstado;
         this.iIdEstado = iIdEstado;
@@ -27,30 +38,73 @@ public class Estado {
     }
 
     //Metodos Getters
+    /*
+    * getsNombreEstado
+    *
+    * Metodo para recuper el nombre del objecto.
+    * @return sNombreEstado es el <code>Nombre<code> del objecto.
+     */
     public String getsNombreEstado() {
         return sNombreEstado;
     }
 
+    /*
+    * getiIdEstado
+    *
+    * Metodo para recuper el ID del objecto.
+    * @return iIdEstado es el <code>ID<code> del objecto.
+     */
     public int getiIdEstado() {
         return iIdEstado;
     }
 
+    /*
+    * getgBibBibliotecasActuales
+    *
+    * Metodo para recuper el gestor de libros del objecto.
+    * @return gBibBibliotecasActuales es el <code>Gestor de Libros<code> del objecto.
+     */
     public Gestor_Biblioteca getgBibBibliotecasActuales() {
         return gBibBibliotecasActuales;
     }
 
     //Metodos Setters
+    /*
+    * setiIdEstado
+    *
+    * Metodo modificador el ID del objecto.
+    * @param iIdEstado es el <code>ID<code> del objecto.
+     */
     public void setiIdEstado(int iIdEstado) {
         this.iIdEstado = iIdEstado;
     }
 
+    /*
+    * setsNombreEstado
+    *
+    * Metodo modificador del nombre del objecto.
+    * @param sNombreEstado es el <code>Nombre<code> del objecto.
+     */
     public void setsNombreEstado(String sNombreEstado) {
         this.sNombreEstado = sNombreEstado;
     }
 
+    /*
+    * setgBibBibliotecasActuales
+    *
+    * Metodo modificador el gestor de libros del objecto.
+    * @param gBibBibliotecasActuales es el <code>Gestor de Bibliotecas<code> del objecto.
+     */
     public void setgBibBibliotecasActuales(Gestor_Biblioteca gBibBibliotecasActuales) {
         this.gBibBibliotecasActuales = gBibBibliotecasActuales;
     }
-    
-    
+    //Metodos
+    /*
+    * Imprimir
+    *
+    * Metodo para imprimir la informacion perrtinente al objecto
+     */
+    public void Imprimir(){
+        System.out.println("Id: " + this.getiIdEstado() + "Nombre: " + this.getsNombreEstado());
+    }
 }
--- Proyecto.Biblioteca/src/proyecto/biblioteca/Libro.java
+++ Proyecto.Biblioteca/src/proyecto/biblioteca/Libro.java
@@ -3,11 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto.biblioteca;
 
 /**
  *
- * @author Adrian
+ * @author Java
  */
 public class Libro {
     private String sNombre,sAutor;
@@ -15,6 +14,16 @@ public class Libro {
     private boolean bRentado;
     private Biblioteca bibPertenezco;
 
+    /*
+    * Libro
+    *
+    * Metodo para la creacion de un objecto Libro sin parametros.
+    * @param sNombre es el <code>Nombre<code> del objecto.
+    * @param sAutor es el <code>Autor<code> del objecto.
+    * @param iISBN es el <code>ISBN<code> del objecto.
+    * @param iAnio es el <code>Anio<code> del objecto.
+    * @param bRentado es el <code>Estatus de rentando<code> del objecto.
+     */
     public Libro() {
         this.sNombre = "";
         this.sAutor = "";
@@ -23,7 +32,17 @@ public class Libro {
         this.bRentado = false;
         this.bibPertenezco = new Biblioteca();
     }
-    
+
+    /*
+    * Libro
+    *
+    * Metodo para la creacion de un objecto Libro dados ciertos parametros.
+    * @param sNombre es el <code>Nombre<code> del objecto.
+    * @param sAutor es el <code>Autor<code> del objecto.
+    * @param iISBN es el <code>ISBN<code> del objecto.
+    * @param iAnio es el <code>Anio<code> del objecto.
+    * @param bRentado es el <code>Estatus de rentando<code> del objecto.
+     */
     public Libro(String sNombre, String sAutor, int iISBN, int iAnio, boolean bPrestado) {
         this.sNombre = sNombre;
         this.sAutor = sAutor;
@@ -33,53 +52,141 @@ public class Libro {
     }
 
     //Metodos Getters
+    /*
+    * getsNombre
+    *
+    * Metodo para recuperar el nombre del objecto.
+    * @return sNombre es el <code>Nombre<code> del objecto.
+     */
     public String getsNombre() {
         return sNombre;
     }
 
+    /*
+   * getsAutor
+   *
+   * Metodo para recuperar el autor del objecto.
+   * @return sAutor es el <code>Autor<code> del objecto.
+    */
     public String getsAutor() {
         return sAutor;
     }
 
+    /*
+    * getiISBN
+    *
+    * Metodo para recuperar el ISBN del objecto.
+    * @return iISBN es el <code>ISBN<code> del objecto.
+     */
     public int getiISBN() {
         return iISBN;
     }
 
+    /*
+    * getiAnio
+    *
+    * Metodo para recuperar el anio del objecto.
+    * @return iAnio es el <code>Anio<code> del objecto.
+     */
     public int getiAnio() {
         return iAnio;
     }
 
+    /*
+    * isbRentado
+    *
+    * Metodo para recuperar el estatus de si esta rentando o no del objecto.
+    * @return bRentado es el <code>Estatus de rentando<code> del objecto.
+     */
     public boolean isbRentado() {
         return bRentado;
     }
 
+    /*
+    * getBibPertenezco
+    *
+    * Metodo para recuperar la biblioteca al que pertenece objecto.
+    * @return bibPertenezco es el <code>Biblioteca<code> al que pertenece el objecto.
+     */
     public Biblioteca getBibPertenezco() {
         return bibPertenezco;
     }
 
     //Metodos Setters
+    /*
+    * setsNombre
+    *
+    * Metodo modificador del nombre del objecto.
+    * @param sNombre es el <code>Nombre<code> del objecto.
+     */
     public void setsNombre(String sNombre) {
         this.sNombre = sNombre;
     }
 
+    /*
+    * setsAutor
+    *
+    * Metodo modificador del autor del objecto.
+    * @param sAutor es el <code>Autor<code> del objecto.
+     */
     public void setsAutor(String sAutor) {
         this.sAutor = sAutor;
     }
 
+    /*
+    * setiISBN
+    *
+    * Metodo modificador del ISBN del objecto.
+    * @param iISBN es el <code>ISBN<code> del objecto.
+     */
     public void setiISBN(int iISBN) {
         this.iISBN = iISBN;
     }
 
+    /*
+    * setiAnio
+    *
+    * Metodo modificador del anio del objecto.
+    * @param iAnio es el <code>Anio<code> del objecto.
+     */
     public void setiAnio(int iAnio) {
         this.iAnio = iAnio;
     }
 
+    /*
+    * setbRentado
+    *
+    * Metodo modificador del estatus de si esta rentando o no del objecto.
+    * @param bRentado es el <code>Estatus de rentando<code> del objecto.
+     */
     public void setbRentado(boolean bRentado) {
         this.bRentado = bRentado;
     }
 
+    /*
+    * setBibPertenezco
+    *
+    * Metodo modificador de la biblioteca al que pertenece objecto.
+    * @param bibPertenezco es el <code>Biblioteca<code> al que pertenece el objecto.
+     */
     public void setBibPertenezco(Biblioteca bibPertenezco) {
         this.bibPertenezco = bibPertenezco;
     }
-    
+
+    //Metodos
+    /*
+    * Imprimir
+    *
+    * Metodo para imprimir la informacion perrtinente al objecto
+     */
+    public void Imprimir(){
+        System.out.print("ISBN: " + this.getiISBN() + "Nombre: " + this.getsNombre() + "Autor: " +
+                this.getsAutor() + "Año: " + this.getiAnio() + "Estatus: ");
+        if (this.isbRentado()){
+            System.out.print("Rentado\n");
+        }
+        else {
+            System.out.print("Disponible\n");
+        }
+    }
 }
