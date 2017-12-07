86b3cb619f13ed2543134ce60ca2fe34b54e1c8e
lorenavi <lorenavargasislas@gmail.com>
2017-12-07 15:15:12 -0600

    Update Gestor_Estado.java

--- Proyecto.Biblioteca/src/proyecto/biblioteca/Gestor_Estado.java
+++ Proyecto.Biblioteca/src/proyecto/biblioteca/Gestor_Estado.java
@@ -7,30 +7,35 @@ package proyecto.biblioteca;
 
 /**
  *
- * @author Java
+ * @author Lorena
  */
 public class Gestor_Estado {
-    
+
+// Se crea un array para almacenar estados
 ArrayList <Estado> listadeestados = new ArrayList<>();
 
+    //Metodo AgregarE para agregar estados a la lista
     public void agregarE(Estado est){ 
            listadeestados.add(est);
     }
-    
-public void eliminarE(int id){
+
+    //Metodo Eliminar para buscar estado por id y eliminarlo de la lista
+    public void eliminarE(int id){
        int index = 0;
        for(Estado estActual: listadeestados){
            if(id ==estActual.getId()){ 
+               //aviso para el usuario de que ya fue eliminado
                System.out.println(estActual.getNombreEstado() + " se ha removido");
                listadeestados.remove(index);
                return;
            }
            index++;
        }
+        //en caso de no encontrarse el estado en la base de datos segpun su id
        System.out.println("No existe el estado con id: " + id);
        return;
    }
-    
+    //Metodo para imprimir el estado en turno y su información
     public void mostrarE(Estado est){  
 
         for (Estado estActual : listadeestadoss) { 
