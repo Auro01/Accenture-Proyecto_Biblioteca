d794dfee673f9ef192533d3a0103dd4edbee86d4
guitardragoon <adrianmoncar@hotmail.com>
2017-12-06 22:59:48 -0600

    agregar/borrar
    Método agregar y borrar han sido agregados, la funcionalidad aún sin agregar datos.

--- Proyecto.Biblioteca/src/proyecto/biblioteca/Gestor_Estado.java
+++ Proyecto.Biblioteca/src/proyecto/biblioteca/Gestor_Estado.java
@@ -5,10 +5,33 @@
  */
 package proyecto.biblioteca;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Java
  */
 public class Gestor_Estado {
+    ArrayList<Estado> estados = new ArrayList<>();
     
+    
+    public void agregarEstado(Estado edo){
+        estados.add(edo);
+        System.out.println("Estado agregado correctamente");
+    }
+    
+    public int borrarEstado(int id){
+        int index = 0;
+        for(Estado edo: estados){
+            if(id == edo.getiIdEstado()){ 
+                System.out.println("Estado: " + edo.getsNombreEstado() + " fue removido exitosamente");
+                estados.remove(index);
+                return 1;
+           }
+           index++;
+       }
+       System.out.println("No existe el usuario con id: " + id);
+       return 0;
+        
+    }
 }
